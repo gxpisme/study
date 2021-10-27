637cab8d540de1cc56768fd3a2adb47c0d0851ab
gxpisme <gxpisme@gmail.com>
2021-10-27 21:07:06 +0800

    leetcode answer

--- leetcode/src/io/github/gxpisme/TwentyTwo.java
+++ leetcode/src/io/github/gxpisme/TwentyTwo.java
@@ -21,18 +21,21 @@ public class TwentyTwo {
 
     public static void main(String[] args) {
         long start = System.currentTimeMillis();
-        System.out.println(generateParenthesisTwo(1));
-        System.out.println(generateParenthesisTwo(2));
-        System.out.println(generateParenthesisTwo(3));
-        System.out.println(generateParenthesisTwo(4));
-        System.out.println(generateParenthesisTwo(5));
+        List<String> strings = generateParenthesisThree(8);
+        System.out.println(strings);
         System.out.println(System.currentTimeMillis() - start);
 
-        System.out.println(generateParenthesis(1));
-        System.out.println(generateParenthesis(2));
-        System.out.println(generateParenthesis(3));
-        System.out.println(generateParenthesis(4));
-        System.out.println(generateParenthesis(5));
+//        System.out.println(generateParenthesisTwo(1));
+//        System.out.println(generateParenthesisTwo(2));
+//        System.out.println(generateParenthesisTwo(3));
+//        System.out.println(generateParenthesisTwo(4));
+//        System.out.println(generateParenthesisTwo(5));
+
+//        System.out.println(generateParenthesis(1));
+//        System.out.println(generateParenthesis(2));
+//        System.out.println(generateParenthesis(3));
+//        System.out.println(generateParenthesis(4));
+//        System.out.println(generateParenthesis(5));
     }
 
     /**
@@ -181,5 +184,38 @@ public class TwentyTwo {
             }
         }
         return set.stream().distinct().collect(Collectors.toList());
+    }
+
+    /**
+     * ¹Ù·½µÄ´ð°¸
+     * @param n
+     * @return
+     */
+    public static List<String> generateParenthesisThree(int n) {
+        List<String> combinations = new ArrayList();
+        generateAll(new char[2 * n], 0, combinations);
+        return combinations;
+    }
+
+    public static void generateAll(char[] current, int pos, List<String> result) {
+        if (pos == current.length) {
+            if (valid(current))
+                result.add(new String(current));
+        } else {
+            current[pos] = '(';
+            generateAll(current, pos+1, result);
+            current[pos] = ')';
+            generateAll(current, pos+1, result);
+        }
+    }
+
+    public static boolean valid(char[] current) {
+        int balance = 0;
+        for (char c: current) {
+            if (c == '(') balance++;
+            else balance--;
+            if (balance < 0) return false;
+        }
+        return (balance == 0);
     }
 }
