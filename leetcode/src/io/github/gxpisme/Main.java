46c087c5bf7296aecdb97e9f312b0f0e9a77b96b
gxpisme <gxpisme@gmail.com>
2021-10-29 17:02:03 +0800

    4sumII

--- leetcode/src/io/github/gxpisme/Main.java
+++ leetcode/src/io/github/gxpisme/Main.java
@@ -1,9 +1,20 @@
 package io.github.gxpisme;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        List<String> strings = Arrays.asList("a", "b", "c");
+
+        for (int i = 0; i < strings.size(); i++) {
+            for (int j = i + 1; j < strings.size(); j++) {
+                for (int k = j + 1; k < strings.size(); k++) {
+                    System.out.println(strings.get(i) + strings.get(j) + strings.get(k));
+                }
+            }
+        }
     }
 }
